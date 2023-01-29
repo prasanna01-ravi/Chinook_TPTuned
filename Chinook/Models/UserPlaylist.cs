ea1366f2cde76f109b66a6c8c175a3e248437d53
R.Prasanna <prasanna01.raveendran@gmail.com>
2023-01-29 23:19:37 +0530

    Folder Structure updated

--- Chinook/Models/UserPlaylist.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chinook.Models;
-
-public class UserPlaylist
-{
-    public string UserId { get; set; }
-    public long PlaylistId { get; set; }
-    public ChinookUser User { get; set; }
-    public Playlist Playlist { get; set; }
-}
